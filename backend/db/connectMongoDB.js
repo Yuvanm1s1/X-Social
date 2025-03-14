3d8388284d5c55f81f58cec2d4a3c42e586061f1
John Doe <johndoe@email.com>
2025-03-14 10:01:24 +0530

    1-project setup

---
+++ backend/db/connectMongoDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const connectMongoDB=async()=>{
+    try{
+        const conn = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`Connection sucessfull , ${conn.connection.host}`);
+    }catch(error){
+        console.log(error.message);
+        process.exit(1);
+    }
+}
+export default connectMongoDB;
---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import authRoutes from "./routes/auth.routes.js"
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
+dotenv.config();
+console.log("MongoDB URI:", process.env.MONGO_URI); // Debugging
+const app = express();
+const PORT = process.env.PORT || 8000; // Ensure a valid port
+
+app.use("/api/auth",authRoutes);
+
+app.listen(PORT, "0.0.0.0", () => { // Explicitly define the host
+  console.log(`Server running on port ${PORT}`);
+  connectMongoDB();
+});
