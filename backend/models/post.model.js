c3dab37c6085fe62e747beda4dbd0f48327ce809
John Doe <johndoe@email.com>
2025-04-19 19:00:49 +0530

    3- delete posts

--- backend/models/post.model.js
+++ backend/models/post.model.js
@@ -1,27 +1,69 @@
 import mongoose from "mongoose";
-const postSchema = mongoose.Schema({
-    user:{
-        type:mongoose.Schema.Types.ObjectId,
-        ref:'User',
-        required:true
-    },
-    img:{
-        type:String,
+// const postSchema = mongoose.Schema({
+//     user:{
+//         type:mongoose.Schema.Types.ObjectId,
+//         ref:'User',
+//         required:true
+//     },
+//     img:{
+//         type:String,
       
-    },
-    likes:[
-        {type:mongoose.Schema.Types.ObjectId,ref:'User',required:true}
-    ],
-    comments:[{
-        text:{
-            type:String,
-            required:true
-        },
-        user:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
+//     },
+//     likes:[
+//         {type:mongoose.Schema.Types.ObjectId,ref:'User',required:true}
+//     ],
+//     comments:[{
+//         text:{
+//             type:String,
+//             required:true
+//         },
+//         user:{type:mongoose.Schema.Types.ObjectId,ref:'User',required:true},
 
-    }]
-},{
-    timestamps:true
+//     }]
+// },{
+//     timestamps:true
+// });
+
+// const Post = mongoose.model('Post',postSchema);
+// export default Post;
+
+
+
+const postSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    text: {
+        type: String,
+        trim: true // optional but useful to remove leading/trailing spaces
+    },
+    img: {
+        type: String,
+    },
+    likes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        }
+    ],
+    comments: [
+        {
+            text: {
+                type: String,
+                required: true
+            },
+            user: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'User',
+                required: true
+            }
+        }
+    ]
+}, {
+    timestamps: true
 });
 
 const Post = mongoose.model('Post',postSchema);
